95826e30d600d227c59114fff694c2356c6f6b31
Ali Xudiyev <ali.xudiyev98@gmail.com>
2024-03-28 12:26:35 +0400

    LabTask14 added

---
+++ LabTask#14/Product.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask_14
+{
+    internal class Product
+    {
+        public int Id;
+        public string BrandName;
+        public string Model;
+        public int Price;
+        public int Cost;
+        public int InCome;
+        public int Count;
+
+        public Product(int id,string brandName,string model,int price,int cost,int count)
+        {
+            Id = id;
+            BrandName = brandName;
+            Model = model;
+            Price = price;
+            Cost = cost;
+            InCome = 0;
+          
+            if (count>0)
+            {
+            Count = count;
+            }else Count= 0;
+            if (Count > 0)
+            {
+                InCome += (Price - Cost);
+                Count--;
+                Console.WriteLine("Id--BrandName--Model--Price--Cost--Income--Count");
+                Console.WriteLine(Id + " " + BrandName + " " + Model + " " + Price + " " + Cost + " " + InCome + " " + Count);
+            }
+            else
+            {
+                Console.WriteLine("Mehsul bitib");
+            }
+        }
+
+        public void GetInfo(Product product)
+        {
+            Console.WriteLine("Id--BrandName--Model--Price--Cost--Income--Count");
+          Console.WriteLine(product.Id+" "+product.BrandName+" "+product.Model+" "+product.Price+" "+product.Cost+" "+product.InCome+" "+Count);
+        }
+
+    
+
+    }
+}
---
+++ LabTask#14/Program.cs
@@ -0,0 +1,17 @@
+﻿using LabTask_14;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Product product = new(1, "Lenovo", "ideapad 5", 1200, 50, 10);
+        Product product1 = new(1, "Asus", "Rog", 1000, 50, 0);
+        Product product2 = new(1, "Hp", "Pavilion", 1500, 60, 5);
+        product.GetInfo(product);
+        
+
+
+
+
+    }
+}
